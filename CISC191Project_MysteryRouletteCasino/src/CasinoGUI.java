7f01cc8c9a8949803b16af2f31354b52ac6f3765
Jordi Mejia <jordi.j.mejia@gmail.com>
2022-12-09 18:42:03 -0800

    design updates

--- CISC191Project_MysteryRouletteCasino/src/CasinoGUI.java
+++ CISC191Project_MysteryRouletteCasino/src/CasinoGUI.java
@@ -10,8 +10,8 @@ public class CasinoGUI extends JFrame{
 	private static final long serialVersionUID = 1L;
 	
 	//A few constants for the size of the window
-	private final static int WINDOW_WIDTH = 900;
-	private final static int WINDOW_HEIGHT = 1000;
+	private final static int WINDOW_WIDTH = 1800;
+	private final static int WINDOW_HEIGHT = 1200;
     
 //    private int[] x = {265, 265,
 //                286, 307, 327, 344, 359, 372, 381, 388, 390, 389, 385, 377,
@@ -45,7 +45,7 @@ public class CasinoGUI extends JFrame{
     private int rotation = 0;
     
     private int positionX = 265;
-    private int positionY = 165;
+    private int positionY = 240;
     
     private int card = 0;
     
@@ -53,10 +53,26 @@ public class CasinoGUI extends JFrame{
     private CasinoPanel casinoPanel = new CasinoPanel();
 	
 	
-    private static ImageIcon img1 = new ImageIcon("/Users/jordi.j.mejiacruz/eclipse-workspace/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Roulette.png");
-    private static ImageIcon img2 = new ImageIcon("/Users/jordi.j.mejiacruz/eclipse-workspace/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/BettingTable.png");
-    private static ImageIcon img3 = new ImageIcon("/Users/jordi.j.mejiacruz/eclipse-workspace/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/MysteryRouletteCards/BackCardTemplate.png");
+    private static ImageIcon img1 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Roulette.png");
+    private static ImageIcon img2 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/BettingTable.png");
+    private static ImageIcon img3 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/MysteryRouletteCards/BackCardTemplate.png");
 
+    private static ImageIcon header = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonCasinoRoulette.png");
+    private static ImageIcon author = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/Neon_Jordino.png");
+
+    private static ImageIcon chip1 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonChip1.png");
+    private static ImageIcon chip5 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonChip5.png");
+    private static ImageIcon chip10 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonChip10.png");
+    private static ImageIcon chip25 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonChip25.png");
+    private static ImageIcon chip100 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonChip100.png");
+    private static ImageIcon chip500 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonChip500.png");
+    
+    private static ImageIcon p1Icon = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonSpade.png");
+    private static ImageIcon p2Icon = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonHeart.png");
+    private static ImageIcon p3Icon = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonClub.png");
+    private static ImageIcon p4Icon = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/Signs/NeonDiamond.png");
+
+    
     
     private MysteryRoulette newGame = new MysteryRoulette();
     private Roulette win = new MysteryRoulette();
@@ -65,6 +81,15 @@ public class CasinoGUI extends JFrame{
    
     
     JPanel playerPanel = new JPanel();
+    
+    private static JLabel casinoNeonSign;
+    private static JLabel jordinoNeonSign;
+    private static JLabel c1;
+    private static JLabel c5;
+    private static JLabel c10;
+    private static JLabel c25;
+    private static JLabel c100;
+    private static JLabel c500;
     
     private static JLabel wheel;
     private static JLabel betTable;
@@ -75,6 +100,11 @@ public class CasinoGUI extends JFrame{
     private static JLabel sign1;
     private static JLabel sign2;
     private static JLabel sign3;
+    
+    private static JLabel player1;
+    private static JLabel player2;
+    private static JLabel player3;
+    private static JLabel player4;
     
     
     private static JButton startButton;
@@ -94,11 +124,24 @@ public class CasinoGUI extends JFrame{
         
         
         //playerPanel.setBounds(900, 0, 600, 1000);
+        casinoNeonSign = new JLabel(header);
+        jordinoNeonSign = new JLabel(author);
         wheel = new JLabel(img1);
         betTable = new JLabel(img2);
+        player1 = new JLabel(p1Icon);
+        player2 = new JLabel(p2Icon);
+        player3 = new JLabel(p3Icon);
+        player4 = new JLabel(p4Icon);
         sign1 = new JLabel();
         sign2 = new JLabel();
         sign3 = new JLabel();
+        c1 = new JLabel(chip1);
+        c5 = new JLabel(chip5);
+        c10 = new JLabel(chip10);
+        c25 = new JLabel(chip25);
+        c100 = new JLabel(chip100);
+        c500 = new JLabel(chip500);
+        
 
         
         sign1.setText("0<=SPIN<=12");
@@ -109,21 +152,43 @@ public class CasinoGUI extends JFrame{
         sign3.setForeground(new Color(206, 194, 144)); 
         
         
-        wheel.setBounds(100, 125, 350, 350);
-        betTable.setBounds(100, 550, 700, 340);
-        sign1.setBounds(620, 147, 100, 10);
-        sign2.setBounds(620, 300, 100, 10);
-        sign3.setBounds(620, 453, 100, 10);
+        casinoNeonSign.setBounds(750, 0, 300, 150);
+        wheel.setBounds(100, 200, 350, 350);
+        betTable.setBounds(880, 150, 700, 340);
+        player1.setBounds(100, 700, 75, 75);
+        player2.setBounds(500, 700, 75, 75);
+        player3.setBounds(900, 700, 75, 75);
+        player4.setBounds(1300, 700, 75, 82);
+        sign1.setBounds(620, 222, 100, 10);
+        sign2.setBounds(620, 375, 100, 10);
+        sign3.setBounds(620, 528, 100, 10);
+        c1.setBounds(893, 500, 112, 80);
+        c5.setBounds(1005, 500, 112, 80);
+        c10.setBounds(1117, 500, 112, 80);
+        c25.setBounds(1229, 500, 112, 80);
+        c100.setBounds(1341, 500, 112, 80);
+        c500.setBounds(1455, 500, 112, 80);
         
         
 		gameGUI();
 		
         
+		casinoPanel.add(casinoNeonSign);
         casinoPanel.add(wheel);
         casinoPanel.add(betTable);
+        casinoPanel.add(player1);
+        casinoPanel.add(player2);
+        casinoPanel.add(player3);
+        casinoPanel.add(player4);
         casinoPanel.add(sign1);
         casinoPanel.add(sign2);
         casinoPanel.add(sign3);
+        casinoPanel.add(c1);
+        casinoPanel.add(c5);
+        casinoPanel.add(c10);
+        casinoPanel.add(c25);
+        casinoPanel.add(c100);
+        casinoPanel.add(c500);
         
         
         add(casinoPanel);
@@ -141,9 +206,9 @@ public class CasinoGUI extends JFrame{
         card2 = new JLabel(img3);
         card3 = new JLabel(img3);
         
-        card1.setBounds(500, 75, 100, 145);
-        card2.setBounds(500, 228, 100, 145);
-        card3.setBounds(500, 381, 100, 145);
+        card1.setBounds(500, 150, 100, 145);
+        card2.setBounds(500, 303, 100, 145);
+        card3.setBounds(500, 456, 100, 145);
         
         casinoPanel.add(card1);
         casinoPanel.add(card2);
@@ -168,16 +233,23 @@ public class CasinoGUI extends JFrame{
 		public void actionPerformed(ActionEvent e) {
 			try {
 				if(rotation == 15 && position[index] == wheelLandsOn.getNumber()){
+					System.out.println("Roulette Wheel!");
+					System.out.println("Winning Number: " + wheelLandsOn.getNumber() + " " + wheelLandsOn.getColor());
+					System.out.println();
+					System.out.println("Mystery Roulette Card!");
 					System.out.println("Winning Number: " + win.getNumber() + " " + win.getColor());
+					System.out.println();
 					timer.stop();
 					
 					card1.setIcon(img3);
 					card2.setIcon(img3);
 					card3.setIcon(img3);
 					
+					ImageIcon img4 = new ImageIcon("/Users/jordi.j.mejiacruz/git/repository/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/MysteryRouletteCards/FrontCard_" + win.getNumber() + ".png");
+
+					
 					if(card == 1){
 						timer.restart();
-						ImageIcon img4 = new ImageIcon("/Users/jordi.j.mejiacruz/eclipse-workspace/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/MysteryRouletteCards/FrontCard_" + win.getNumber() + ".png");
 						card1.setIcon(img4);
 						
 						
@@ -199,7 +271,6 @@ public class CasinoGUI extends JFrame{
 						
 					}else if(card == 2){
 						timer.restart();
-						ImageIcon img4 = new ImageIcon("/Users/jordi.j.mejiacruz/eclipse-workspace/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/MysteryRouletteCards/FrontCard_" + win.getNumber() + ".png");
 						card2.setIcon(img4);
 						
 						TimeUnit.SECONDS.sleep(2);
@@ -222,7 +293,6 @@ public class CasinoGUI extends JFrame{
 						
 					}else if(card == 3){
 						timer.restart();
-						ImageIcon img4 = new ImageIcon("/Users/jordi.j.mejiacruz/eclipse-workspace/CISC191Project_MysteryRouletteCasino/src/Roulette_Imgs/MysteryRouletteCards/FrontCard_" + win.getNumber() + ".png");
 						card3.setIcon(img4);
 						
 						TimeUnit.SECONDS.sleep(2);
@@ -244,12 +314,6 @@ public class CasinoGUI extends JFrame{
 						
 						
 					}
-					
-
-					
-//					System.out.println("Winning Number: " + win.getNumber() + " " + win.getColor());
-//					timer.stop();
-					
 				}else if (index == 36) {
                     positionX += moveXTo[index];
                     positionY += moveYTo[index]; 
